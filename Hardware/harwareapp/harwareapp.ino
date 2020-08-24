db10ce2660be485d271061d6fa038923d757c21f
0x6f736f646f <rodneydav@gmail.com>
2020-08-24 22:19:21 +0300

    ✨ Function to read light intensity #2

--- Hardware/harwareapp/harwareapp.ino
+++ Hardware/harwareapp/harwareapp.ino
@@ -4,20 +4,23 @@
 int loop_time = 5000;
 const int DHT_PIN = PA6;
 const int soilMoisturePin = PB1;
+const int ldrPin = PB0;
 
-float humidity, temperature, soilMoistureValue;
+float humidity, temperature, soilMoistureValue, ldrValue;
 
 DHT dht(DHT_PIN, DHTTYPE);
 
 void readTemp(void);
 void readHum(void);
 void readSoilMoisture(void);
+void readLightIntensity(void);
 
 void setup()
 {
     Serial.begin(9600);
     Serial.println("Starting DHT11");
     pinMode(soilMoisturePin, INPUT);
+    pinMode(ldrPin, INPUT);
     dht.begin();
 }
 
@@ -26,6 +29,7 @@ void loop()
     readTemp();
     readHum();
     readSoilMoisture();
+    readLightIntensity();
     delay(loop_time);
     // Wait for a few seconds to loop
 }
@@ -61,7 +65,16 @@ void readSoilMoisture(void)
     // Read soil moisture
     soilMoistureValue = analogRead(soilMoisturePin);
     soilMoistureValue = map(soilMoistureValue, 0, 4095, 0, 100);
-    // Map value : 400 will be 100 and 1023 will be 0
+    // Map value : 0 will be 0 and 4095 will be 1000
     Serial.print("Soil moisture content: ");
     Serial.println(soilMoistureValue);
+}
+
+void readLightIntensity(void)
+{
+    int ldrValue = analogRead(ldrPin);
+    ldrValue = map(ldrValue, 0, 4095, 100, 0);
+    // Map value : 0 will be 100 and 4095 will be 0
+    Serial.print("Light intensity Value: ");
+    Serial.println(ldrValue);
 }
