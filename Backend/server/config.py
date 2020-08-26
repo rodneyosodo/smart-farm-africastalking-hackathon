9acde48d528badd07bc6a131eca6e26450acc4c3
0x6f736f646f <blackd0t@protonmail.com>
2020-08-26 23:37:14 +0300

    ✨ App config

---
+++ Backend/server/config.py
@@ -0,0 +1,28 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] # export DATABASE_URL="postgresql://postgres:postgres@127.0.0.1:1001/postgres"
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
